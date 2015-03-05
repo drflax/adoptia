1b65bb707b892d0c3ddbbcdaf2234ddcb0f57c1f
Flax Prodcció <flavio.nottalgiovanni@gmail.com>
2015-03-05 16:49:23 +0100

    new update

--- adoptar.php
+++ adoptar.php
@@ -1,50 +1,40 @@
-<?php  
-//conectamos con la base de datos 
+<?php 
+require "PDO_Pagination.php";
 
-$con = mysqli_connect("mysql.hostinger.es", "u997080380_drf", "sambucetti", "u997080380_adopt"); 
+/* Config Connection */
+$root = 'u997080380_drf';
+$password = 'sambucetti';
+$host = 'mysql.hostinger.es';
+$dbname = 'u997080380_adopt';
 
-
-//Definimos variables de consulta
-
- $postal = $_POST['postal'];
-  
- $hld = $_POST['hld'];
-  
- $animal = $_POST['animal']; 
-
-//establecemos condiciones de paginacion 
-$registros = 1; 
-
-@$pagina = $_GET ['pagina']; 
-
-if (!isset($pagina)) 
-{ 
-$pagina = 1; 
-$inicio = 0; 
-} 
-else 
-{ 
-$inicio = ($pagina-1) * $registros; 
-} 
-
-//realizamos la busqueda en la base de datos 
-$sql = " SELECT FROM adopt(foto, email, telf, postal, nomcont, edad, nomanimal, funciones, hld, neces, animal) WHERE postal LIKE  '$postal' WHERE hld <='$hld WHERE animal LIKE  '$animal' "; 
-$cad = mysqli_query($con,$sql); 
-
-//calculamos las paginas a mostrar 
-
-$contar = "SELECT (foto, email, telf, postal, nomcont, edad, nomanimal, funciones, hld, neces, animal) FROM adopt"; 
-$contarok = mysqli_query($con,$contar); 
-$total_registros = mysqli_num_rows($contarok); 
-$total_paginas = ($total_registros / $registros); 
-
-//imprimiendo los resultados 
+$connection = new PDO("mysql:host=$host;dbname=$dbname;", $root, $password);
+$pagination = new PDO_Pagination($connection);
 
 
-while ($array = mysqli_fetch_array($cad)) 
+$postal = $_REQUEST['postal'];
+$hld = $_REQUEST['hld']; 
+$animal = $_REQUEST['animal'];
 
-/* ==============================================*/ 
-
+$pagination->param = "&postal=$postal&hld=$hld&animal=$animal";
+$pagination->rowCount("SELECT  * FROM `adopt` WHERE `postal` LIKE '$postal' AND `hld`<= '$hld' AND `animal` LIKE '$animal'");
+$pagination->config(2, 1);
+$sql = "SELECT  `animal`, `email`, `telf`, `postal`, `nomcont`, `edad`, `funciones`, `hld`, `neces`, `idfoto`, `foto`, `nomanimal`, `pass` FROM `adopt` WHERE `postal` LIKE '$postal' AND `hld`<= '$hld' AND `animal` LIKE '$animal' LIMIT $pagination->start_row, $pagination->max_rows";
+$query = $connection->prepare($sql);
+$query->execute();
+$model = array();
+while($model = $query->fetch())
+{  
+ $nomanimal = $model['nomanimal'];
+ $edad = $model['edad'];
+ $animal = $model['animal'];
+ $hld = $model['hld'];
+ $funciones = $model['funciones'];
+ $neces = $model['neces'];
+ $nomcont = $model['nomcont'];
+ $email = $model['email'];
+ $foto = $model['foto'];
+ $telf = $model['telf'];
+}
 
 ?> 
 <!doctype html>
@@ -52,7 +42,7 @@ while ($array = mysqli_fetch_array($cad))
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="initial-scale=1.0">
-  <title>dog</title>
+  <title>aDoptar</title>
   <link rel="stylesheet" href="css/standardize.css">
   <link rel="stylesheet" href="css/cats-grid.css">
   <link rel="stylesheet" href="css/styles.css">
@@ -60,32 +50,58 @@ while ($array = mysqli_fetch_array($cad))
 <body class="body page-cats clearfix">
   <p onClick="window.location='select.html';" class="text text-2">aDoptia</p>
   <div class="container container-1 clearfix">
-    <div class="container container-2 clearfix">
-      <p class="text text-4">Nombre:</p>
-      <input id="nomanimal" value="<?php echo $array['nomanimal'];?>" class="_input _input-2 js-nomanimal" name="nomanimal" type="text">
-      <p class="text text-6">Edad:</p>
-      <input id="edad" value="<?php echo $array['edad'];?>"class="_input _input-4 js-edad" name="edad" type="text">
-      <p class="text text-8">Raza:</p>
-      <input id="animal" value="<?php echo $array['animal'];?>"class="_input _input-6 js-animal" name="animal" type="text">
-      <p class="text text-10">HLD:</p>
-      <input id="hld" value="<?php echo $array['hld'];?>"class="_input _input-8 js-hld" name="hld" type="text">
-      <p class="text text-12">Funciones:</p>
-      <input id="funciones" value="<?php echo $array['funciones'];?>"class="_input _input-10 js-funciones" name="funciones" type="text">
-      <p class="text text-14">Nece.Esp:</p>
-      <input id="neces" value="<?php echo $array['neces'];?>"class="_input _input-12 js-neces" name="neces" type="text">
-      <p class="text text-16">Contacto:</p>
-      <input id="nomcont" value="<?php echo $array['nomcont'];?>"class="_input _input-14 js-nomcont" name="nomcont" type="text">
-      <p class="text text-19">Email:</p>
-      <input id="email" value="<?php echo $array['email'];?>"class="_input _input-16 js-email" name="email" type="text">
-      <p class="text text-22">Telefono:</p>
-      <input id="telf" value="<?php echo $array['telf'];?>"class="_input _input-18 js-telf" name="telf" type="text">
-    </div>
-    <input id="foto" value="<?php echo $array['foto'];?>" class="_input _input-20 js-foto" name="foto" type="text">
+     <div class="container container-2 clearfix">
+          <p class="text text-4">Nombre:</p>
+          <input id="nomanimal" read_only="read_only" value=" <?php echo $nomanimal; ?> " class="_input _input-2 js-nomanimal" name="nomanimal" type="text">
+          <p class="text text-6">Edad:</p>
+          <input id="edad" read_only="read_only" value="<?php echo $edad; ?>" class="_input _input-4 js-edad" name="edad" type="text">
+          <p class="text text-8">Raza:</p>
+          <input id="animal" read_only="read_only" value="<?php echo $animal; ?>" class="_input _input-6 js-animal" name="animal" type="text">
+          <p class="text text-10">HLD:</p>
+          <input id="hld" read_only="read_only" value="<?php echo $hld; ?>" class="_input _input-8 js-hld" name="hld" type="text">
+          <p class="text text-12">Caracter:</p>
+          <input id="funciones" read_only="read_only" value="<?php echo $funciones; ?>" class="_input _input-10 js-funciones" name="funciones" type="text">
+          <p class="text text-14">Nece.Esp:</p>
+          <input id="neces" read_only="read_only" value="<?php echo $neces; ?>" class="_input _input-12 js-neces" name="neces" type="text">
+          <p class="text text-16">Contacto:</p>
+          <input id="nomcont" read_only="read_only" value="<?php echo $nomcont; ?>" class="_input _input-14 js-nomcont" name="nomcont" type="text"> 
+          <p class="text text-19">Email:</p>
+          <input id="email" read_only="read_only" value="<?php echo $email; ?>" class="_input _input-16 js-email" name="email" type="text">
+          <p class="text text-22">Telefono:</p>
+          <input id="telf" read_only="read_only" value="<?php echo $telf;  ?>" class="_input _input-18 js-telf" name="telf" type="text">
+      </div>
+          <img src="<?php echo $foto;  ?>" class="_input _input-20 js-foto" name="foto"  >
   </div>
-  <button onClick="window.location='tel:$telf';" id="adoptar" class="_button-1 js-adoptar">Adoptar</button>
-  <button onClick="window.location='adoptar.php?pagina=".($pagina-1)."';" id="sigu" class="_button-4 js-sigu">No Adoptar</button>
+          <button onClick="window.location='tel:<?php echo $telf; ?>';" id="adoptar" class="_button-1 js-adoptar">Adoptar</button>
+                  
 
   <script src="js/jquery-min.js"></script>
+  <style>
+            /* CSS */
+            .btn
+            {
+              text-decoration: none;
+              color: #FFFFFF;
+              padding-left: 10px;
+              padding-right: 10px;
+              margin-left: 1px;
+              margin-right: 1px;
+              border-radius: 3px;
+              background: #7F83AD;
+            }
+            
+            .btn:hover
+            {
+                background: #474C80;
+            }
+            .active
+            {
+                background: #E7814A;
+            }
+            /* CSS */
+  </style>
 </body>
 </html>
-  
+<?php
+$pagination->pages("btn");
+?>
